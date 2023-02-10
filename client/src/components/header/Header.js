10951951a25ef866a3c00cbcaeb64f1c66995817
Abdelrahman-Ibrahem <79354542+Abdo-112002@users.noreply.github.com>
2023-02-09 16:10:58 -0800

    header-component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { StyledHeader, StyledLogo } from './header.style'
+
+
+function Header() {
+  return (
+    <StyledHeader>
+        <StyledLogo>
+            shopping cart
+        </StyledLogo>
+    </StyledHeader>
+  )
+}
+
+export default Header
---
+++ client/src/components/min-content/MinContent.style.js
@@ -0,0 +1,10 @@
+
+
+import styled from 'styled-components';
+
+export const StyledMinSection = styled.section`
+    grid-area: main;
+    
+`;
+
+
