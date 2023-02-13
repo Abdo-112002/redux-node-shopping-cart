e203ea3e35af025d591d0f7732dfb324586cf2d2
Abdelrahman-Ibrahem <79354542+Abdo-112002@users.noreply.github.com>
2023-02-13 06:16:49 -0800

    filter-comp

---
+++ client/src/components/filter/filter.style.js
@@ -0,0 +1,48 @@
+
+
+import styled from "styled-components";
+import { CartTitle } from "../cart/cart.style";
+
+export const FilterWrapper = styled.div`
+    padding: 50px 0;
+    position: relative;
+`;
+
+export const FilterContent = styled.div`
+    background-color: #ddd;
+    padding: 10px;
+    border-radius: 5px;
+    position: sticky;
+    top: 0;
+    left: 0;
+`;
+
+export const FilterTitle = styled(CartTitle)`
+    color: black;
+    font-weight: bold;
+    margin: 15px 0;
+`;
+
+export const FilterTypesContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 5px;
+    margin: 7px 0;
+`;
+
+export const InputLabel = styled.label.attrs(({name})=> ({
+    for : name,
+}))`
+    color: #2a5ea5;
+`;
+
+export const FilterType = styled.input.attrs(({inputType,name})=> ({
+    type : inputType,
+    name : name,
+}))`
+    font-size: 20px;
+    accent-color:#2a5ea5;
+    cursor: pointer;
+`;
+
